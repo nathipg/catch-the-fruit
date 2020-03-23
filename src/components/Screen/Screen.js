1a4c045f98f2eca12926183c7695b48885431d38
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-03-23 16:24:08 -0300

    Changing Screen component to center content acording to prop centerContent

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -3,8 +3,14 @@ import React from 'react';
 import './Screen.scss';
 
 const Screen = props => {
+    const classes = ['Screen'];
+
+    if(props.centerContent) {
+        classes.push('center');
+    }
+
     return (
-        <div className="Screen">
+        <div className={classes.join(' ')}>
             {props.children}
         </div>
     );
--- src/containers/Start/Start.js
+++ src/containers/Start/Start.js
@@ -3,15 +3,11 @@ import React from 'react';
 import Screen from '../../components/Screen/Screen';
 import ButtonLink from '../../components/ButtonLink/ButtonLink';
 
-import './Start.scss';
-
 const Start = () => {
     return (
-        <Screen>
-            <div className="Start">
-                <h1>Catch the fruit</h1>
-                <ButtonLink to="/select-fruit">Play Game</ButtonLink>
-            </div>
+        <Screen centerContent>
+            <h1>Catch the fruit</h1>
+            <ButtonLink to="/select-fruit">Play Game</ButtonLink>
         </Screen>
     );
 };
